3fb9579dc19d8e268e09b3185fb5aecbbf9d524e
macws <1207460050@qq.com>
2024-12-19 22:34:06 +0800

    gconfig update

--- core/gconfig/config.go
+++ core/gconfig/config.go
@@ -12,19 +12,19 @@ import (
 func NewFlagFile(config interface{}, filepath string, fileUsage string) {
 	configFile := flag.String("f", filepath, fileUsage)
 	flag.Parse()
-	configor.Load(&config, *configFile)
+	configor.Load(config, *configFile)
 }
 
 // load files
 func NewLoadFile(config interface{}, files ...string) {
-	configor.Load(&config, files...)
+	configor.Load(config, files...)
 }
 
 func NewViper(filepath string, filetype string) *viper.Viper {
 	config := viper.New()
 	config.SetConfigType(filetype)
 	config.SetConfigFile(filepath)
-	if err := viper.ReadInConfig(); err != nil {
+	if err := config.ReadInConfig(); err != nil {
 		fmt.Printf("读取配置文件失败: %v\n", err)
 	}
 	return config
