656206cb4d02947b08c241f8d670d567b3502b58
macws <1207460050@qq.com>
2024-12-19 21:42:46 +0800

    gconfig

---
+++ core/gconfig/config.go
@@ -0,0 +1,31 @@
+package gconfig
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/jinzhu/configor"
+	"github.com/spf13/viper"
+)
+
+// load flag file
+func NewFlagFile(config interface{}, filepath string, fileUsage string) {
+	configFile := flag.String("f", filepath, fileUsage)
+	flag.Parse()
+	configor.Load(&config, *configFile)
+}
+
+// load files
+func NewLoadFile(config interface{}, files ...string) {
+	configor.Load(&config, files...)
+}
+
+func NewViper(filepath string, filetype string) *viper.Viper {
+	config := viper.New()
+	config.SetConfigType(filetype)
+	config.SetConfigFile(filepath)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("读取配置文件失败: %v\n", err)
+	}
+	return config
+}
